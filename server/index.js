edbc714f8502d2a4e53205d897b7f57e183d9c3d
Ivan Sabido <isc_86@hotmail.com>
2018-09-29 21:00:42 -0500

    fix

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ let app = express()
 app.use(cors())
 console.log(__dirname)
 app.use(express.static(path.join(__dirname, './dist')))
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 80
 // Server index.html page when request to the root is made
 app.use('/dist', express.static('./dist'))
 app.listen(port)
